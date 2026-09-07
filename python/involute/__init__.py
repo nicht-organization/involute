6085eade53b9219c8b83e5250039f0b5125c1917
xerx593 <alexlutz79@gmail.com>
2026-09-07 14:45:05 +0000

    feat: reorganize imports and update __all__ to include new functions

--- python/involute/__init__.py
+++ python/involute/__init__.py
@@ -1,13 +1,17 @@
-from .schema import InvoluteGate
-from .math.diophantine import CrossEquationUnifier, batch_evaluate_gates
-from .stream.mmap import stream_file
 from .core.ffi import run_involute_eval, run_register_eval
+from .math.diophantine import CrossEquationUnifier, batch_evaluate_gates
+from .math.radical import compute_radical
+from .schema import InvoluteGate
+from .stream.async_mmap import stream_file_async
+from .stream.mmap import stream_file
 
 __all__ = [
     "InvoluteGate",
     "CrossEquationUnifier",
     "batch_evaluate_gates",
+    "compute_radical",
     "stream_file",
+    "stream_file_async",
     "run_involute_eval",
     "run_register_eval",
 ]
